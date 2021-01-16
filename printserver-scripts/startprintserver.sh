a304159c4c9989299d4b5bdd884ff2e502c0f246
weidingerhp <hans-peter@hp-weidinger.at>
2021-01-16 19:17:43 +0100

    tweaks to enable airprint

--- printserver-scripts/startprintserver.sh
+++ printserver-scripts/startprintserver.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
-
 ##### Prepare Users for Cups #####
-
 if [ "${ADMIN}" == "" ]
 then
     ADMIN=admin
-fi 
+fi
 
 if [ "${ADMINPASS}" == "" ]
 then
     ADMINPASS=password
 fi 
 
-useradd -g lpadmin -m "${ADMIN}"
+useradd -g lpadmin -G sudo -m "${ADMIN}"
 
 echo -e "${ADMINPASS}\n${ADMINPASS}" | passwd "${ADMIN}"
 echo -e "${ADMINPASS}\n${ADMINPASS}" | smbpasswd -s -a "${ADMIN}"
 
 #### Start Cups Daemon ######
 service cups start
-
-service dbus start
-service avahi-daemon start
 service cups-browsed start
+service dbus start
+
+##### Update the avahi daemon files #####
+rm /etc/avahi/services/AirPrint-*
+python3 /opt/airprint-generate.py -d /etc/avahi/services/ -v
+
+##### Start all the rest of the services
+service avahi-daemon start
 service ssh start
 
 ##### Start Samba Server #####
